957b904eea863f0b56f7fc2105d0601aa21419c8
annprog <tecbbs@qq.com>
2015-10-30 19:55:25 +0800

    zabbix sendmail script: fix bugs

--- zabbix/sendmail/mail.py
+++ zabbix/sendmail/mail.py
@@ -14,6 +14,7 @@ import smtplib
 from email.mime.text import MIMEText
 import time
 import ConfigParser
+import re
 
 #解析配置文件
 config = ConfigParser.ConfigParser()
@@ -26,7 +27,6 @@ mail_postfix=config.get("mailserver", "postfix")
 mailto_list=config.get("send", "mailto_list").split(",")
 mailcc_list=config.get("send", "mailcc_list").split(",")
 
-toaddrs = mailto_list + mailcc_list
 
 log="/tmp/sendmail.log"
 
@@ -43,25 +43,45 @@ def send_mail(to_list,sub,content):
 	content:内容
 	send_mail("admin@qq.com","sub","content")
 	'''
-	global cc_list
 	address=mail_user+"<"+mail_user+"@"+mail_postfix+">"
 	msg = MIMEText(content, 'plain', 'utf-8')
 	msg['Subject'] = sub
 	msg['From'] = address
+
+	to_list = to_list.split(",")
 	msg['To'] =";".join(to_list)
 	msg['Cc'] =";".join(mailcc_list)
+	toaddrs = to_list + mailcc_list
 	try:
 		s = smtplib.SMTP()
 		s.connect(mail_host)
-		s.login(mail_user,mail_pass)
+		#s.login(mail_user,mail_pass)
 		s.sendmail(address, toaddrs, msg.as_string())
 		s.close()
 		
-		maillog("success", to_list, sub)
+		maillog("success", toaddrs, sub)
 		return True
 	except Exception, e:
 		print str(e)
-		maillog(str(e), to_list, sub)
+		maillog(str(e), toaddrs, sub)
 		return False
+
 if __name__ == '__main__':
-		send_mail(mailto_list, "test", "this is a test message")
+	sub = sys.argv[2].replace("\n", "")
+	sub = sub.replace("\r\n", "")
+	regex = re.compile(r'DETAIL:.*$')
+	sub = regex.sub("DETAIL:", sub)
+
+	to_list = sys.argv[1]
+	to_list = to_list.split(",")
+	new_list = []
+	for to in to_list:
+		if "@" in to:
+			new_list.append(to)
+		else:
+			to = to + "@letv.com"
+			new_list.append(to)
+	new_list = ",".join(new_list)
+	send_mail(new_list, sub, sys.argv[3])
+
+	#send_mail(mailto_list, "test", "this is a test message")
