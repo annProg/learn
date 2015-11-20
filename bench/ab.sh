5d0571a93795c80eb30d7b84cc3306ceaed994c6
annprog <tecbbs@qq.com>
2015-11-20 17:47:24 +0800

    fix ab.sh

--- bench/ab.sh
+++ bench/ab.sh
@@ -17,7 +17,7 @@ n=5000
 
 begin=`date +%m%d-%H:%M:%S`
 
-#for c in {100,200,300,500,1000};do
+#for c in {100,300};do
 for c in {1,10,50,100,200,300,500,1000};do
 	tmplog="$logname/raw-ab-$logname-c$c-n$n.log"
 	gpllog="$logname/gnuplot-ab-$logname-c$c-n$n.log"
@@ -33,7 +33,8 @@ for c in {1,10,50,100,200,300,500,1000};do
 	path=`grep "Document Path" $tmplog |awk '{print $NF}'`
 	request="http://"$hostname$path
 	completeRequest=`grep "Complete requests" $tmplog |awk '{print $NF}'`
-	failedRequest=`grep "Failed requests" $tmplog |awk '{print $NF}'`
+	failedRequest=`grep "Non-2xx responses" $tmplog |awk '{print $NF}'`
+	[ "$failedRequest"x == ""x ] && failedRequest=0
 	tpr=`grep "Time per request" $tmplog |grep -v "concurrent" |awk '{print $4}'`
 	qps=`grep "Requests per second" $tmplog |awk '{print $4}'`
 	less1s=`cat $gpllog | awk '{if($9<1000) sum++}END{print sum-1}'`
@@ -48,14 +49,14 @@ function meanAnalyze()
 	file="$logname/non2xx"
 	cat $logname/ab-* |grep -v "concurrency" |sort -k3 -n >$file.dat
 cat > $file.plt <<EOF
-	set term png size 1366 768
+	set term png size 3000,2000
 	set output "$file.png"
 	set title "total requests $n ($begin - $finish)"
 	set grid
 	set xlabel "concurrency"
 	set ylabel "failed requests"
 	plot "$file.dat" using 3:6 with linespoints pointtype 7 pointsize 2 title "non2xx", \
-		"$file.dat" using 3:7 with linespoints pointtype 7 pointsize 2 title "qps(/s)", \
+		"$file.dat" using 3:(\$7*10) with linespoints pointtype 7 pointsize 2 title "qps*10(/s)", \
 		"$file.dat" using 3:9 with linespoints pointtype 7 pointsize 2 title "less than 1s", \
 		"$file.dat" using 3:8 with linespoints pointtype 7 pointsize 2 title "time per request(ms)"
 EOF
