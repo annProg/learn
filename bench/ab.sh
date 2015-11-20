70fefeca85f4d396ed90a17ec80231457fd3dff6
annprog <tecbbs@qq.com>
2015-11-20 15:14:42 +0800

    ab.sh

--- bench/ab.sh
+++ bench/ab.sh
@@ -15,7 +15,9 @@ logname=`echo $url |awk -F '/' '{print $NF}'`
 [ ! -d $logname ] && mkdir $logname
 n=5000
 
+begin=`date +%m%d-%H:%M:%S`
 
+#for c in {100,200,300,500,1000};do
 for c in {1,10,50,100,200,300,500,1000};do
 	tmplog="$logname/raw-ab-$logname-c$c-n$n.log"
 	gpllog="$logname/gnuplot-ab-$logname-c$c-n$n.log"
@@ -23,7 +25,7 @@ for c in {1,10,50,100,200,300,500,1000};do
 
 	echo > $tmplog
 	startTime=`date +%m%d-%H:%M:%S`
-	ab -c $c -n $n -g $gpllog "$url" > $tmplog
+	ab -k -c $c -n $n -g $gpllog "$url" > $tmplog
 	endTime=`date +%m%d-%H:%M:%S`
 
 	serversoft=`grep "Server Software" $tmplog |awk '{print $NF}'`
@@ -34,25 +36,28 @@ for c in {1,10,50,100,200,300,500,1000};do
 	failedRequest=`grep "Failed requests" $tmplog |awk '{print $NF}'`
 	tpr=`grep "Time per request" $tmplog |grep -v "concurrent" |awk '{print $4}'`
 	qps=`grep "Requests per second" $tmplog |awk '{print $4}'`
-	echo "start end concurrency requests completeRequest failedRequest qps tpr" >$log
-	echo "$startTime $endTime $c $n $completeRequest $failedRequest $qps $tpr" >>$log
+	less1s=`cat $gpllog | awk '{if($9<1000) sum++}END{print sum-1}'`
+	echo "start end concurrency requests completeRequest failedRequest qps tpr less1s" >$log
+	echo "$startTime $endTime $c $n $completeRequest $failedRequest $qps $tpr $less1s" >>$log
 done
 
+finish=`date +%m%d-%H:%M:%S`
 
-function meanStat()
+function meanAnalyze()
 {
 	file="$logname/non2xx"
 	cat $logname/ab-* |grep -v "concurrency" |sort -k3 -n >$file.dat
 cat > $file.plt <<EOF
 	set term png size 1366 768
 	set output "$file.png"
-	set title "total requests $n"
+	set title "total requests $n ($begin - $finish)"
 	set grid
 	set xlabel "concurrency"
 	set ylabel "failed requests"
 	plot "$file.dat" using 3:6 with linespoints pointtype 7 pointsize 2 title "non2xx", \
 		"$file.dat" using 3:7 with linespoints pointtype 7 pointsize 2 title "qps(/s)", \
-		"$file.dat" using 3:(\$8*100) with linespoints pointtype 7 pointsize 2 title "time per request(ms*100)"
+		"$file.dat" using 3:9 with linespoints pointtype 7 pointsize 2 title "less than 1s", \
+		"$file.dat" using 3:8 with linespoints pointtype 7 pointsize 2 title "time per request(ms)"
 EOF
 	gnuplot $file.plt
 }
@@ -62,4 +67,4 @@ function response()
 	file="$logname/response"
 }
 
-meanStat
+meanAnalyze
