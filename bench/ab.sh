d9dcdc4d2f550b15ef50a521b1b45271c520a5e6
annprog <tecbbs@qq.com>
2015-11-20 15:14:55 +0800

    ab.sh

--- bench/ab.sh
+++ bench/ab.sh
@@ -25,7 +25,7 @@ for c in {1,10,50,100,200,300,500,1000};do
 
 	echo > $tmplog
 	startTime=`date +%m%d-%H:%M:%S`
-	ab -k -c $c -n $n -g $gpllog "$url" > $tmplog
+	ab -c $c -n $n -g $gpllog "$url" > $tmplog
 	endTime=`date +%m%d-%H:%M:%S`
 
 	serversoft=`grep "Server Software" $tmplog |awk '{print $NF}'`
