7ca6679a932901b00a0dfc2c40b543dd2ecf109b
annprog <tecbbs@qq.com>
2015-11-24 13:57:39 +0800

    fix bug of timeout.sh

--- bench/timeout.sh
+++ bench/timeout.sh
@@ -8,4 +8,6 @@
 # Created Time: 2015-11-24 12:13:42
 ############################
 
-netstat -at |grep "TIME_WAIT" |awk '/^tcp/{a[$6]++}END{for(i in a){printf("%s\t%d\n",i,a[i])}}'
+t=`date +%m%d-%H:%M:%S`
+tw=`netstat -at |grep "TIME_WAIT" |awk '/^tcp/{a[$6]++}END{for(i in a){printf("%s\t%d\n",i,a[i])}}'`
+echo "$t $tw"
