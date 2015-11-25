8aed71c1111d68e8e0308d43d133037a9cf498c8
annprog <tecbbs@qq.com>
2015-11-25 13:37:38 +0800

    ab.sh增加100ms和500ms统计

--- bench/ab.sh
+++ bench/ab.sh
@@ -38,8 +38,10 @@ for c in {1,10,50,100,200,300,500,1000};do
 	tpr=`grep "Time per request" $tmplog |grep -v "concurrent" |awk '{print $4}'`
 	qps=`grep "Requests per second" $tmplog |awk '{print $4}'`
 	less1s=`cat $gpllog | awk '{if($9<1000) sum++}END{print sum-1}'`
-	echo "start end concurrency requests completeRequest failedRequest qps tpr less1s" >$log
-	echo "$startTime $endTime $c $n $completeRequest $failedRequest $qps $tpr $less1s" >>$log
+	less500ms=`cat $gpllog | awk '{if($9<500) sum++}END{print sum-1}'`
+	less100ms=`cat $gpllog | awk '{if($9<100) sum++}END{print sum-1}'`
+	echo "start end concurrency requests completeRequest failedRequest qps tpr less1s less500ms less100ms" >$log
+	echo "$startTime $endTime $c $n $completeRequest $failedRequest $qps $tpr $less1s $less500ms $less100ms" >>$log
 done
 
 finish=`date +%m%d-%H:%M:%S`
@@ -59,6 +61,8 @@ cat > $file.plt <<EOF
 	plot "$file.dat" using 3:6 with linespoints pointtype 7 pointsize 2 title "non2xx", \
 		"$file.dat" using 3:(\$7*10) with linespoints pointtype 7 pointsize 2 title "qps*10(/s)", \
 		"$file.dat" using 3:9 with linespoints pointtype 7 pointsize 2 title "less than 1s", \
+		"$file.dat" using 3:10 with linespoints pointtype 7 pointsize 2 title "less than 500ms", \
+		"$file.dat" using 3:11 with linespoints pointtype 7 pointsize 2 title "less than 100ms", \
 		"$file.dat" using 3:8 with linespoints pointtype 7 pointsize 2 title "time per request(ms)"
 EOF
 	gnuplot $file.plt
