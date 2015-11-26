381a2ddea019b7e8b8ca0180e7ef29f76d9c2fb2
annprog <tecbbs@qq.com>
2015-11-26 14:20:43 +0800

    ab.sh add timeseries and throughput analyze

--- bench/ab.sh
+++ bench/ab.sh
@@ -14,8 +14,9 @@ option=$4
 n=$2
 comment=$3
 
-logname=`echo $url |awk -F '/' '{print $NF}'`"-"$comment
+logname=`echo $url |awk -F '/' '{print $NF}'`
 [ "$logname"x == ""x ] && logname=`echo $url |awk -F '/' '{print $3}'`
+logname=$logname"_"$comment
 [ ! -d $logname ] && mkdir $logname
 
 begin=`date +%m%d-%H:%M:%S`
@@ -49,15 +50,15 @@ done
 
 finish=`date +%m%d-%H:%M:%S`
 
-function meanAnalyze()
+function overview()
 {
-	file="$logname/non2xx"
+	file="$logname/overview"
 	cat $logname/ab-* |grep -v "concurrency" |sort -k3 -n >$file.dat
 	#comment=`awk '{print $1"-"$2":"$3}'`
 cat > $file.plt <<EOF
-	set term png size 1920,1040
+	set term png size 1300,700
 	set output "$file.png"
-	set title "total requests $n ($begin - $finish)\n$comment"
+	set title "total requests $n ($begin - $finish)\n\n$comment"
 	set grid
 	set xlabel "concurrency"
 	set ylabel "requests count"
@@ -71,9 +72,65 @@ EOF
 	gnuplot $file.plt
 }
 
-function response()
+function timeSeries()
 {
-	file="$logname/response"
+	datadir="$logname/timeSeries"
+	[ ! -d "$datadir" ] && mkdir "$datadir"
+	for id in `ls $logname/gnuplot-*`;do
+		title=`echo $id |cut -f2 -d'/' |sed 's/\.log//g'`
+		file=$datadir"/"$title
+		cat >$file.plt <<EOF
+		set term png size 500,500
+		set size 1,1
+		set output "$file.png"
+		set title "timeSeries of $title"
+		set grid
+		set xdata time
+		set timefmt "%s"
+		set format x "%S"
+		set xlabel 'seconds'
+		set ylabel "response time (ms)"
+		set datafile separator '\\t'
+		plot "$id" every ::2 using 2:5 title 'response time' with points
+EOF
+		gnuplot $file.plt
+	done
 }
 
-meanAnalyze
+function throughput()
+{
+	datadir="$logname/throughput"
+	[ ! -d $datadir ] && mkdir "$datadir"
+	for id in `ls $logname/gnuplot-*`;do
+		title=`echo $id |cut -f2 -d'/' |sed 's/\.log//g'`
+		file=$datadir"/"$title
+		awk '{print $6}' $id |grep -v wait |sort |uniq -c|awk '{print $2,$1}' >$file.dat
+	done
+	
+	plt="$datadir/throughput.plt"
+
+	cat >$plt <<EOF
+	set term png size 1300,700
+	set output "$datadir/throughput.png"
+	set title "$logname ($begin - $finish)\n\n$comment"
+	set grid
+	set key invert reverse Left outside
+	set xdata time
+	set timefmt "%s"
+	set format x "%S"
+	set xlabel "seconds"
+	set ylabel "responses per second"
+EOF
+	echo -n "plot " >>$plt
+	for id in `ls $datadir/gnuplot-*`;do
+		concurrency=`echo $id | awk -F '-' '{print $(NF-1)}'`
+		echo "\"$id\" using 1:2 title \"concurrency $concurrency\" with linespoints pointtype 7 pointsize 2, \\" >> $plt
+	done
+	
+	sed -i '$s/,.*//' $plt
+	gnuplot $plt
+}
+
+overview
+timeSeries
+throughput
