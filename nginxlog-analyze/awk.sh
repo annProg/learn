54b60b0ea24e15ac55ff0f5ee0203496be310b89
annprog <tecbbs@qq.com>
2015-12-16 23:21:55 +0800

    nginxlog awk

--- nginxlog-analyze/awk.sh
+++ nginxlog-analyze/awk.sh
@@ -8,4 +8,10 @@
 # Created Time: 2015-12-16 17:54:52
 ############################
 
-
+logfile=$1
+# 日志格式
+# '$remote_addr [$time_iso8601]  $status $body_bytes_sent '
+# '"$request_method $scheme://$server_name$request_uri" '
+# '"$http_referer" "$http_user_agent" "$http_x_forwarded_for" '
+# '"$request_time" "$upstream_addr" "$upstream_status" "$upstream_response_time"';
+awk '{print $}
