d7e65e3b6ea3af789fb77b804b4e4b1426ed0096
annprog <tecbbs@qq.com>
2015-12-16 18:06:58 +0800

    用awk分析nginx日志

---
+++ nginxlog-analyze/awk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+############################
+# Usage:
+# File Name: awk.sh
+# Author: annhe  
+# Mail: i@annhe.net
+# Created Time: 2015-12-16 17:54:52
+############################
+
+
