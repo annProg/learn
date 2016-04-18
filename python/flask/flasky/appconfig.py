eaf299b4dae5371cdb2c65328d5ee5b7c8443cb5
annprog <tecbbs@qq.com>
2016-04-18 18:49:53 +0800

    [python flask] 学习 Flask-WTForm

---
+++ python/flask/flasky/appconfig.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-  
+
+############################
+# Usage:
+# File Name: appconfig.py
+# Author: annhe  
+# Mail: i@annhe.net
+# Created Time: 2016-04-18 16:04:41
+############################
+
+config = {}
+config['SECRET_KEY'] = '2837MLmklsfdk283u98'
--- python/flask/flasky/hello.py
+++ python/flask/flasky/hello.py
@@ -9,18 +9,37 @@
 # Created Time: 2016-04-14 16:03:52
 ############################
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from datetime import datetime
+import appconfig
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
+app.config['SECRET_KEY'] = appconfig.config['SECRET_KEY']
+class NameForm(Form):
+	name = StringField('姓名', validators = [Required()])
+	submit = SubmitField('提交')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return(render_template('index.html'))
+	form = NameForm()
+	if form.validate_on_submit():
+		old_name = session.get('name')
+		if old_name is not None and old_name != form.name.data:
+			flash('似乎您改变了您的用户名!')
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	
+	return(render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name')))
 
 @app.route('/user/<name>')
 def user(name):
