e65bd61ca141016d40c8231f3fd560a057668a8c
annprog <tecbbs@qq.com>
2016-04-19 19:20:41 +0800

    [python flask] 学习数据库

--- python/flask/flasky/hello.py
+++ python/flask/flasky/hello.py
@@ -10,14 +10,19 @@
 ############################
 
 from flask import Flask, render_template, session, redirect, url_for, flash
-from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.script import Manager, Shell
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime
 import appconfig
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 manager = Manager(app)
@@ -25,21 +30,58 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 app.config['SECRET_KEY'] = appconfig.config['SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
+
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(64), unique = True)
+	users = db.relationship('User', backref='role')
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key = True)
+	username = db.Column(db.String(64), unique = True, index = True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
 class NameForm(Form):
 	name = StringField('姓名', validators = [Required()])
 	submit = SubmitField('提交')
+
+def make_shell_context():
+	return dict(app=app,db=db,User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	form = NameForm()
 	if form.validate_on_submit():
-		old_name = session.get('name')
-		if old_name is not None and old_name != form.name.data:
-			flash('似乎您改变了您的用户名!')
+		user = User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user = User(username = form.name.data)
+			db.session.add(user)
+			session['known'] = False
+		else:
+			session['known'] = True
+
 		session['name'] = form.name.data
+		form.name.data = ''
 		return redirect(url_for('index'))
 	
-	return(render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name')))
+	return(render_template('index.html', known = session.get('known', False), form=form, name=session.get('name')))
 
 @app.route('/user/<name>')
 def user(name):
---
+++ python/flask/flasky/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-  
+
+############################
+# Usage:
+# File Name: test.py
+# Author: annhe  
+# Mail: i@annhe.net
+# Created Time: 2016-04-19 13:56:30
+############################
+
+import os
+
+dirname = os.path.dirname(__file__)
+abspath = os.path.abspath(dirname)
+
+print(dirname)
+print(abspath)
