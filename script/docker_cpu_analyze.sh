48a475c10a3c426f377d39285c28b112a3fb9a71
annprog <tecbbs@qq.com>
2016-04-21 11:59:30 +0800

    [script] fix docker_cpu_analyze.sh

--- script/docker_cpu_analyze.sh
+++ script/docker_cpu_analyze.sh
@@ -8,9 +8,17 @@
 # Created Time: 2016-04-20 20:09:48
 ############################
 
-#!/bin/bash
+function color()
+{
+    echo -e "\033[41m$1\033[0m"
+}
+
+echo "CONTAINER-ID   PORT  %CPU  MESOS_TASK_ID"
 for id in `docker ps |awk 'NR!=1{print $1}'`;do 
-    app=`docker inspect $id |grep -w Hostname |cut -f4 -d'"'`
-    echo -n "$id - $app - ";docker top $id aux|awk 'NR!=1{sum+=$3}END{print sum}'
+    mesosid=`docker inspect $id | grep -w "MESOS_TASK_ID" |cut -f2 -d'=' |tr -d '",'`
+    port=`docker inspect $id |grep -w "PORT0" |cut -f2 -d'=' |tr -d '",'`
+    cpu=`docker top $id aux|awk 'NR!=1{sum+=$3}END{print sum}'`
+    #echo "$id  $port  `color $cpu`  $mesosid"
+    printf "%s  %s  %-15s%s\n" "$id" "$port" "`color $cpu`" "$mesosid"  
 done
 
