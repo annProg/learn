6dc97e700b8edcf7ee19b5648af61b540811220e
annprog <tecbbs@qq.com>
2016-05-03 17:23:23 +0800

    [python flask] 确认账户

--- python/flask/flasky/app/auth/views.py
+++ python/flask/flasky/app/auth/views.py
@@ -10,11 +10,34 @@
 ############################
 
 from flask import render_template, redirect, request, url_for, flash
-from flask.ext.login import login_user, logout_user, login_required
+from flask.ext.login import login_user, logout_user, login_required, current_user
 from . import auth
 from ..models import User
 from .forms import LoginForm, RegistrationForm
 from .. import db
+from ..email import send_mail
+
+@auth.before_app_request
+def before_app_request():
+	if current_user.is_authenticated \
+			and not current_user.confirmed \
+			and request.endpoint[:5] != 'auth.' \
+			and request.endpoint != 'static':
+		return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+	if current_user.is_anonymous or current_user.confirmed:
+		return redirect(url_for('main.index'))
+	return render_template('auth/unconfirmed.html')
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+	token = current_user.generate_confirmation_token()
+	send_mail(current_user.email, '激活邮件', 'auth/email/confirm', user=current_user, token=token)
+	flash('激活邮件已重新发送')
+	return redirect(url_for('main.index'))
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -40,6 +63,22 @@ def register():
 	if form.validate_on_submit():
 		user = User(email=form.email.data, username=form.username.data, password=form.password.data)
 		db.session.add(user)
-		flash('注册成功, 请登录')
+		db.session.commit()
+
+		token = user.generate_confirmation_token()
+		send_mail(user.email, '邮件激活', 'auth/email/confirm', user=user, token=token)
+		flash('激活邮件已发送到您的邮箱，请点击邮件中的链接激活您的账号')
 		return redirect(url_for('auth.login'))
 	return render_template('auth/register.html', form=form)
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+	if current_user.confirmed:
+		return redirect(url_for('main.index'))
+	if current_user.confirm(token):
+		flash('账号已激活，感谢您的注册')
+	else:
+		flash('激活失败。链接错误或者失效')
+	return redirect(url_for('main.index'))
--- python/flask/flasky/app/models.py
+++ python/flask/flasky/app/models.py
@@ -13,6 +13,8 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.login import UserMixin
 from . import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
 class Role(db.Model):
 	__tablename__ = 'roles'
@@ -30,6 +32,7 @@ class User(UserMixin, db.Model):
 	username = db.Column(db.String(64), unique = True, index = True)
 	password_hash = db.Column(db.String(128))
 	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+	confirmed = db.Column(db.Boolean, default=False)
 
 	@property
 	def password(self):
@@ -41,6 +44,24 @@ class User(UserMixin, db.Model):
 	
 	def verify_password(self, password):
 		return check_password_hash(self.password_hash, password)
+
+	def generate_confirmation_token(self, expiration=300):
+		s = Serializer(current_app.config['SECRET_KEY'], expiration)
+		return s.dumps({'confirm': self.id})
+
+	def confirm(self, token):
+		s = Serializer(current_app.config['SECRET_KEY'])
+		try:
+			data = s.loads(token)
+		except:
+			return False
+
+		if data.get('confirm') != self.id:
+			return False
+		self.confirmed = True
+		db.session.add(self)
+		db.session.commit()
+		return True
 
 	def __repr__(self):
 		return '<User %r>' % self.username
