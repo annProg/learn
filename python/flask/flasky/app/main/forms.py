2b66a0a280f9e24215136bde3765e4dfea11b623
annprog <tecbbs@qq.com>
2016-05-04 18:41:43 +0800

    [python flask] 用户资料

--- python/flask/flasky/app/main/forms.py
+++ python/flask/flasky/app/main/forms.py
@@ -10,10 +10,43 @@
 ############################
 
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms import StringField, TextAreaField, SubmitField, BooleanField, SelectField
+from wtforms.validators import Required, Length, Email, Regexp
+from wtforms import ValidationError
+from ..models import Role, User
 
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
+class EditProfileForm(Form):
+	name = StringField('姓名', validators=[Length(0, 64)])
+	location = StringField('位置', validators=[Length(0, 64)])
+	about_me = TextAreaField('自我介绍')
+	submit = SubmitField('更新')
+
+class EditProfileAdminForm(Form):
+	email = StringField('邮箱', validators=[Required(), Length(1, 64), Email()])
+	username = StringField('用户名', validators=[Required(), Length(1,64), Regexp('^[A-za-z0-9_.]*$',0,'用户名包含非法字符')])
+	confirmed = BooleanField('验证状态')
+	role = SelectField('Role', coerce=int)
+	name = StringField('姓名', validators=[Length(0,64)])
+	location = StringField('位置', validators=[Length(0,64)])
+	about_me = TextAreaField('自我介绍')
+	submit = SubmitField('更新')
+
+	def __init__(self, user, *args, **kwargs):
+		super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+		self.role.choices = [(role.id, role.name) for role in Role.query.order_by(Role.name).all()]
+		self.user = user
+
+	def validate_email(self, field):
+		if field.data != self.user.email and \
+				User.query.filter_by(email=field.data).first():
+			raise ValidationError('邮箱已被占用')
+	
+	def validate_username(self, field):
+		if field.data != self.user.username and \
+				User.query.filter_by(username=field.data).first():
+			raise ValidationError('用户名已被占用')
--- python/flask/flasky/app/main/views.py
+++ python/flask/flasky/app/main/views.py
@@ -9,13 +9,14 @@
 # Created Time: 2016-04-20 19:56:33
 ############################
 
-from flask import render_template, session, redirect, url_for, current_app
+from flask import render_template, session, redirect, url_for, current_app, flash
 from .. import db
-from ..models import User
+from ..models import User, Role
 from ..email import send_mail
 from . import main
-from .forms import NameForm
-from flask.ext.login import login_required
+from .forms import EditProfileForm, NameForm, EditProfileAdminForm
+from flask.ext.login import login_required, current_user
+from ..decorators import admin_required
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -34,7 +35,57 @@ def index():
 		return redirect(url_for('.index'))
 	return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
+@main.route('/user/<username>')
+def user(username):
+	user = User.query.filter_by(username=username).first()
+	if user is None:
+		abort(404)
+	return render_template('user.html', user=user)
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+	form = EditProfileForm()
+	if form.validate_on_submit():
+		current_user.name = form.name.data
+		current_user.location = form.location.data
+		current_user.about_me = form.about_me.data
+		db.session.add(current_user)
+		db.session.commit()
+		flash('个人资料已更新')
+		return redirect(url_for('.user', username=current_user.username))
+	form.name.data = current_user.name
+	form.location.data = current_user.location
+	form.about_me.data = current_user.about_me
+	return render_template('edit_profile.html', form=form)
+
 @main.route('/secret')
 @login_required
 def secret():
 	return '登录用户可见'
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+	user = User.query.get_or_404(id)
+	form = EditProfileAdminForm(user=user)
+	if form.validate_on_submit():
+		user.email = form.email.data
+		user.username = form.username.data
+		user.confirmed = form.confirmed.data
+		user.role = Role.query.get(form.role.data)
+		user.name = form.name.data
+		user.location = form.location.data
+		user.about_me = form.about_me.data
+		db.session.add(user)
+		flash('The profile has been updated.')
+		return redirect(url_for('.user', username=user.username))
+	form.email.data = user.email
+	form.username.data = user.username
+	form.confirmed.data = user.confirmed
+	form.role.data = user.role_id
+	form.name.data = user.name
+	form.location.data = user.location
+	form.about_me.data = user.about_me
+	return render_template('edit_profile.html', form=form, user=user)
