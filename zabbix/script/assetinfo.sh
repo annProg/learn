fca8a2c217ce30ea0a143fc52080f7acb804df9e
annprog <tecbbs@qq.com>
2016-10-08 19:03:34 +0800

    [zabbix]fix assetinfo.sh

--- zabbix/script/assetinfo.sh
+++ zabbix/script/assetinfo.sh
@@ -8,18 +8,17 @@
 # Created Time: 2016-09-29 15:11:36
 ############################
 
-sn=`dmidecode -s system-serial-number|grep -v "^#"`
-uuid=`dmidecode -s system-uuid|grep -v "^#"`
-manufacturer=`dmidecode -s system-manufacturer|grep -v "^#"`
-product=`dmidecode -s system-product-name|grep -v "^#"`
+sn=`sudo dmidecode -s system-serial-number |grep -v "^#"`
+uuid=`sudo dmidecode -s system-uuid |grep -v "^#"`
+manufacturer=`sudo dmidecode -s system-manufacturer|grep -v "^#"`
+product=`sudo dmidecode -s system-product-name|grep -v "^#"`
 
 echo $sn |grep -E " |-" &>/dev/null && assettag=$uuid || assettag=$sn
 
-str="{\n\t\"data\":["
-for id in assettag manufacturer product;do
-	cmd="echo \$$id"
-	str="$str\n\t\t{\"{#TAG}\":\"$id\", \"{#VALUE}\":\"`eval $cmd`\"},"
-done
-str="$str]\n}"
-str=`echo $str |sed 's/,]/\\\\n\\\\t]/g'`
-echo -e $str
+case $1 in
+	"sn") echo $assettag;;
+"manufacturer") echo $manufacturer;;
+"product") echo $product;;
+*) exit;;
+esac
+
