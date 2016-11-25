6fe05f0e3cb1ff9b7c87a0d0c26b59f030965f80
annprog <tecbbs@qq.com>
2016-11-25 18:53:13 +0800

    :smile:

--- golang/tmp/sqrt.go
+++ golang/tmp/sqrt.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"time"
 )
 
 func Sqrt(x float64) float64 {
@@ -13,5 +15,12 @@ func Sqrt(x float64) float64 {
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	t1 := time.Now()
+	fmt.Println(Sqrt(7000))
+	t2 := time.Now()
+	fmt.Println(math.Sqrt(7000))
+	t3 := time.Now()
+
+	fmt.Println(t2.Sub(t1))
+	fmt.Println(t3.Sub(t2))
 }
