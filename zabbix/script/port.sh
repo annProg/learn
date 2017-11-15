8dc92b8389200394483cba3256976cded5524c45
annprog <tecbbs@qq.com>
2017-11-15 18:09:44 +0800

    [zabbix/script]port.sh改用netstat采集端口信息

--- zabbix/script/port.sh
+++ zabbix/script/port.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 exclude="zabbix_agentd|monit|snmpd|sendmail|moxi"
 conf="/etc/lld_port.conf"
 
@@ -9,20 +8,28 @@ hostname | grep "mpaas_node" &>/dev/null && r=0 || r=1
 hostname | grep ".kafka." &>/dev/null && r=0 || r=1
 [ $r -eq 0 ] && include="9092:java|sshd|dnsmasq|agentd_zx"
 
+hostname | grep -E "ad.*.(log-collection|bigdata)" &>/dev/null && r=0 || r=1
+[ $r -eq 0 ] && exclude=$exclude"|java"
+
 if [ -f $conf ];then
     source $conf
 fi
 
 str='{\n\t"data":['
 
+function ntlp()
+{
+	netstat -ntlp |awk 'NR>2{print $4"#"$7}' |sed -r 's/.*?:([0-9]+)#/\1#/g' |sed -r 's/#[0-9]+?\/(.*)/:\1/g' |grep -v "#"
+}
+
 if [ "$include"x != ""x ];then
-	for id in `sudo ss -lpn |awk '{print $4,$6}' |sed 's/::ffff://g' |perl -pe 's/.*?:(\d)/\1/' |sed 's/users:((\"//g'|sed 's/\".*$//' |grep -vE "127.0.0.1|172.17.|Local" |sort -u |grep -v ":" |tr ' ' ':' | grep -v ":$" | grep -Ew "$include"`;do
+	for id in `ntlp | grep -Ew "$include"`;do
 	    proc=`echo $id | cut -f2 -d':'`
 	    port=`echo $id | cut -f1 -d':'`
 	    str="$str\n\t\t{\"{#PROCNAME}\":\"$proc\", \"{#PORT}\":\"$port\"},"
 	done
 else
-	for id in `sudo ss -lpn |awk '{print $4,$6}' |sed 's/::ffff://g' |perl -pe 's/.*?:(\d)/\1/' |sed 's/users:((\"//g'|sed 's/\".*$//' |grep -vE "127.0.0.1|172.17.|Local" |sort -u |grep -v ":" |tr ' ' ':' |grep -v ":$" |grep -vEw "$exclude"`;do
+	for id in `ntlp |grep -vEw "$exclude"`;do
 	    proc=`echo $id | cut -f2 -d':'`
 	    port=`echo $id | cut -f1 -d':'`
 	    str="$str\n\t\t{\"{#PROCNAME}\":\"$proc\", \"{#PORT}\":\"$port\"},"
