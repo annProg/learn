0cfd4079c98a8c37137c0e25c5d3b0808068c21e
annProg <tecbbs@qq.com>
2019-09-19 23:17:19 +0800

    gobyexample. range.go

---
+++ golang/gobyexample/range.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+import "reflect"
+
+func main() {
+
+	// nums 是一个 slice
+	nums := []int{2, 3, 4}
+	fmt.Println(reflect.TypeOf(nums))
+    sum := 0
+    for _, num := range nums {
+        sum += num
+    }
+    fmt.Println("sum:", sum)
+
+    for i, num := range nums {
+        if num == 3 {
+            fmt.Println("index:", i)
+        }
+    }
+
+    kvs := map[string]string{"a": "apple", "b": "banana"}
+    for k, v := range kvs {
+        fmt.Printf("%s -> %s\n", k, v)
+    }
+
+    for k := range kvs {
+        fmt.Println("key:", k)
+    }
+
+    for i, c := range "go" {
+        fmt.Println(i, c)
+    }
+}
