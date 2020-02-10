291d2032a8fceec6c50cac3a342c615c539a9139
annProg <tecbbs@qq.com>
2020-02-10 18:13:30 +0800

    [shell] allitebooks. support proxy

--- shell/allitebooks/download.sh
+++ shell/allitebooks/download.sh
@@ -2,11 +2,17 @@
 
 [ ! -d download ] && mkdir download
 
+PROXY=""
+if [ "$1"x != ""x ];then
+	PROXY="--http-proxy=$1"
+fi
+
 cd download
 cat ../pages/*.txt | \
 	aria2c.exe --conditional-get=true \
 	--auto-file-renaming=false \
 	-c \
+	$PROXY \
 	--log-level=notice \
 	--log=download.log \
 	-i -
