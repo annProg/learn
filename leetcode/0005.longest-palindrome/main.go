de332fe2c88b12f20e0e19ae6090424af8fc827f
annProg <tecbbs@qq.com>
2021-06-24 23:54:39 +0800

    leetcode 5

--- leetcode/0005.longest-palindrome/main.go
+++ leetcode/0005.longest-palindrome/main.go
@@ -78,12 +78,39 @@ func longestPalindrome2(s string) string {
 	return s[begin : begin+maxLen]
 }
 
+func longestPalindrome3(s string) string {
+	if s == "" {
+		return ""
+	}
+	start, end := 0, 0
+	for i := 0; i < len(s); i++ {
+		left1, right1 := expandAroundCenter(s, i, i)
+		left2, right2 := expandAroundCenter(s, i, i+1)
+		if right1-left1 > end-start {
+			start, end = left1, right1
+		}
+		if right2-left2 > end-start {
+			start, end = left2, right2
+		}
+	}
+	return s[start : end+1]
+}
+
+func expandAroundCenter(s string, left, right int) (int, int) {
+	for ; left >= 0 && right < len(s) && s[left] == s[right]; left, right = left-1, right+1 {
+	}
+	return left + 1, right - 1
+}
+
 func main() {
-	s := "abcdefghijklmnopqrstuvwxyzabcdefghabcba"
+	s := "abcdefghijklmnopqrstuvwxyzxwvutsrqponmlkjihgfedcba"
 	start := time.Now()
 	longestPalindrome2(s)
 	fmt.Println(time.Since(start))
 	start2 := time.Now()
 	longestPalindrome(s)
 	fmt.Println(time.Since(start2))
+	start3 := time.Now()
+	longestPalindrome3(s)
+	fmt.Println(time.Since(start3))
 }
