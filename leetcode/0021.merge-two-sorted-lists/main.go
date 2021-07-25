9d01eb6749b0de059fe02d0dd934f6488241c939
annprog <tecbbs@qq.com>
2021-07-25 21:55:59 +0800

    leetcode 21

--- leetcode/0021.merge-two-sorted-lists/main.go
+++ leetcode/0021.merge-two-sorted-lists/main.go
@@ -41,3 +41,18 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	}
 	return head
 }
+
+//递归
+func mergeTwoLists2(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	} else if l2 == nil {
+		return l1
+	} else if l1.Val <= l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
+	}
+}
