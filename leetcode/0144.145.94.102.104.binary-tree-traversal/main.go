dbbf5bd2bd5df8850ee927089fdea735184843c4
annprog <tecbbs@qq.com>
2021-07-27 13:09:44 +0800

    leetcode binary tree traversal

--- leetcode/0144.145.94.102.104.binary-tree-traversal/main.go
+++ leetcode/0144.145.94.102.104.binary-tree-traversal/main.go
@@ -42,6 +42,51 @@ func postorderTraversal(root *TreeNode) []int {
 	return ret
 }
 
+func levelOrder(root *TreeNode) [][]int {
+	ret := [][]int{}
+	if root == nil {
+		return ret
+	}
+	que := []*TreeNode{}
+	level := []*TreeNode{}
+	que = append(que, root)
+
+	for len(que) > 0 {
+		for len(que) > 0 {
+			level = append(level, que[len(que)-1])
+			que = que[0 : len(que)-1]
+		}
+		vals := []int{}
+		for len(level) > 0 {
+			node := level[len(level)-1]
+			level = level[0 : len(level)-1]
+			vals = append(vals, node.Val)
+			if node.Left != nil {
+				que = append(que, node.Left)
+			}
+			if node.Right != nil {
+				que = append(que, node.Right)
+			}
+		}
+		ret = append(ret, vals)
+	}
+	return ret
+}
+
+func maxDepth(root *TreeNode) int {
+	depth := 0
+	if root == nil {
+		return depth
+	}
+	depth += 1
+	left := depth + maxDepth(root.Left)
+	right := depth + maxDepth(root.Right)
+	if left >= right {
+		return left
+	}
+	return right
+}
+
 func main() {
 	tree := &TreeNode{Val: 1}
 	tree.Right = &TreeNode{Val: 2}
@@ -49,4 +94,6 @@ func main() {
 	fmt.Println(preorderTraversal(tree))
 	fmt.Println(inorderTraversal(tree))
 	fmt.Println(postorderTraversal(tree))
+	fmt.Println(levelOrder(tree))
+	fmt.Println(maxDepth(tree))
 }
