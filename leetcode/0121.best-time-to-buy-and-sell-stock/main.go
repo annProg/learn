45a30683008029a11b9aa53989d4a1dc3e9070bc
annProg <tecbbs@qq.com>
2021-08-17 12:01:51 +0800

    leetcode 121, offer63

--- leetcode/0121.best-time-to-buy-and-sell-stock/main.go
+++ leetcode/0121.best-time-to-buy-and-sell-stock/main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// 当天的利润加上之前的利润
 func maxProfit(prices []int) int {
 	max := 0
 	pre := max
@@ -10,6 +11,7 @@ func maxProfit(prices []int) int {
 		if today+pre > max {
 			max = pre + today
 		}
+
 		pre += today
 		if pre < 0 {
 			pre = 0
@@ -18,6 +20,20 @@ func maxProfit(prices []int) int {
 	return max
 }
 
+func maxProfit2(prices []int) int {
+	minprice := 1 << 31
+	maxprofit := 0
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < minprice {
+			minprice = prices[i]
+		} else if prices[i]-minprice > maxprofit {
+			maxprofit = prices[i] - minprice
+		}
+	}
+	return maxprofit
+}
+
 func main() {
 	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
+	fmt.Println(maxProfit2([]int{7, 1, 5, 3, 6, 4}))
 }
