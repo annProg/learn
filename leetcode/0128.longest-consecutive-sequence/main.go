01f66a25b220c0ef7a435e16f96953cde21fb0c9
annProg <tecbbs@qq.com>
2021-08-23 17:37:42 +0800

    leetcode 128 hashmap

--- leetcode/0128.longest-consecutive-sequence/main.go
+++ leetcode/0128.longest-consecutive-sequence/main.go
@@ -33,6 +33,35 @@ func longestConsecutive(nums []int) int {
 	return max
 }
 
+func longestConsecutive2(nums []int) int {
+	m := map[int]int{}
+
+	max := 0
+	for _, v := range nums {
+		if dictGet(m, v) == 0 {
+			left := dictGet(m, v-1)
+			right := dictGet(m, v+1)
+
+			cur := 1 + left + right
+
+			if cur > max {
+				max = cur
+			}
+			m[v] = cur
+			m[v-left] = cur
+			m[v+right] = cur
+		}
+	}
+	return max
+}
+
+func dictGet(m map[int]int, v int) int {
+	if _, ok := m[v]; ok {
+		return m[v]
+	}
+	return 0
+}
+
 func main() {
-	fmt.Println(longestConsecutive([]int{1, 2, 0, 1}))
+	fmt.Println(longestConsecutive2([]int{1, 2, 0, 1}))
 }
